912fcdc4581356cc62b9666cc4c92677da20b5c2
pouryourlove <lqu134@naver.com>
2022-10-05 16:45:17 +0900

    Creating add task form

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,23 @@
 import { useState } from "react";
 
+//custom components
+import { CustomForm } from "./components/CustomForm";
+
 function App() {
   const [count, setCount] = useState(0);
 
-  return <div className="App"></div>;
+  const addTask = (task) => {
+    console.log(task);
+  };
+
+  return (
+    <div className="container">
+      <header>
+        <h1>My Task List</h1>
+      </header>
+      <CustomForm addTask={addTask} />
+    </div>
+  );
 }
 
 export default App;
